8afc1095fa44e06fe3a7403521c4bdf0f9110acf
JoeyGrajciar <jozef.grajciar@gmail.com>
2017-11-26 21:48:00 +0100

    support for configuration dot files setup

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ########################################
 # helpers
+########################################
 function print_err {
     local COLOR='\e[91m'
     local NOCOLOR='\e[0m'
@@ -24,5 +25,55 @@ function epilogue {
 
 ########################################
 
+########################################
+# configuration setup functions
+########################################
+
+function config_prepare {
+	prologue ${FUNCNAME[0]}
+	git clone --bare https://github.com/JoeyGrajciar/dotfiles.git $HOME/.cfg-test
+	epilogue ${FUNCNAME[0]}
+}
+
+function config {
+	/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME $@
+}
+
+function config_install {
+	prologue ${FUNCNAME[0]}
+	cd $HOME
+	mkdir -p .config-backup
+	config checkout
+	if [ $? = 0 ]; then
+		echo "Checked out config";
+		else
+		echo "Backing up pre-existing dot files";
+		config checkout 2>$1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+	fi;
+	config checkout
+	config config --local status.showUntrackedFiles no
+	epilogue ${FUNCNAME[0]}
+}
+
+#######################################
+
 print_info "install script"
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+	cfg)
+		CONFIG=1
+		shift
+		;;
+esac
+done
+
+if [[ $CONFIG == 1 ]]; then
+	config_prepare
+	config_install
+fi
+
 print_info "finished"
